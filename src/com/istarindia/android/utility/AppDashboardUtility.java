a9f1e62c559bf0e044e2602addbccdea36ce5945
ravitashaw <ravitashaw@istarindia.com>
2017-04-28 21:42:58 +0530

    assessment header and description

--- src/com/istarindia/android/utility/AppDashboardUtility.java
+++ src/com/istarindia/android/utility/AppDashboardUtility.java
@@ -37,9 +37,15 @@ public class AppDashboardUtility {
 			}
 			taskSummaryPOJO.setTitle(assessment.getAssessmenttitle());
 			taskSummaryPOJO.setImageURL("/root/talentify/assessment.png");
+			taskSummaryPOJO.setHeader(assessment.getAssessmentType());
 			taskSummaryPOJO.setItemPoints(appAssessmentServices.getMaxPointsOfAssessment(assessment.getId()).intValue());
 			taskSummaryPOJO.setNumberOfQuestions(assessment.getAssessmentQuestions().size());
-			taskSummaryPOJO.setDuration(assessment.getAssessmentdurationminutes());			
+			taskSummaryPOJO.setDuration(assessment.getAssessmentdurationminutes());
+			taskSummaryPOJO.setDescription("Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+					+ "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
+					+ "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+					+ "It has survived not only five centuries, but also the leap into electronic typesetting, "
+					+ "remaining essentially unchanged");
 		}
 		return taskSummaryPOJO;
 	}
