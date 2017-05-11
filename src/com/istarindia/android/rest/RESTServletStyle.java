46a115f2e7885ac96d6e44c6622782a4dd090f26
ravitashaw <ravitashaw@istarindia.com>
2017-05-11 22:46:03 +0530

    Redirect basedupn task id

---
+++ src/com/istarindia/android/rest/RESTServletStyle.java
@@ -0,0 +1,49 @@
+package com.istarindia.android.rest;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import com.viksitpro.core.dao.entities.Task;
+import com.viksitpro.core.dao.utils.task.TaskServices;
+import com.viksitpro.core.utilities.TaskCategory;
+
+@Path("get_lesson_details")
+public class RESTServletStyle {
+
+	@Context
+    UriInfo uriInfo;
+	
+	@GET	
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getPOJOOrXML(@QueryParam("userId") int userId, @QueryParam("taskId") int taskId) throws URISyntaxException{
+		
+			TaskServices taskServices = new TaskServices();
+			Task task = taskServices.getTask(taskId);
+			
+			System.out.println(uriInfo.getBaseUri());
+			System.out.println(uriInfo.getAbsolutePath());
+			switch(task.getItemType()){
+			case TaskCategory.LESSON:
+				System.out.println("Task is Video");
+				URI uri = new URI(uriInfo.getBaseUri()+"lessons/user/"+userId+"/"+task.getItemId());
+				ResponseBuilder builder =  Response.seeOther(uri);
+				return builder.build();
+			case TaskCategory.ASSESSMENT:
+				System.out.println("Task is Assessment");
+				URI uriAssessment = new URI(uriInfo.getBaseUri()+"assessments/user/"+userId+"/"+task.getItemId());
+				ResponseBuilder builderAssessment =  Response.seeOther(uriAssessment);
+				return builderAssessment.build();
+			}
+			return null;
+	}
+}
